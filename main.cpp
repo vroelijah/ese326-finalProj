49dd31dee6e53dddb2d53b46098a30cb55966cfa
Elijah Farrell <84297215+vroelijah@users.noreply.github.com>
2023-05-03 16:21:20 -0400

    added main function

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "Cell.h"
+#include "Nets.h"
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+	vector<vector<Cell>> PlacementGrid;
+
+
+
+
+
+}
+
+int Cost (vector<int>old_place,vector<int>new_place){
+
+}
+int cost1() {
+
+}
+int cost2() {
+
+}
+int cost3() {
+
+}
+
+int changeTemp(int& temp) {
+
+}
+void Perturb(vector<vector<Cell>>&grid) {
+
+}
