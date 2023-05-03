131ce2da7ea27cc54e7c05e1c40ad86fda245e6b
Elijah Farrell <84297215+vroelijah@users.noreply.github.com>
2023-05-03 19:10:04 -0400

    added to main

--- main.cpp
+++ main.cpp
@@ -3,15 +3,33 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <map>
+#include <sstream>
 using namespace std;
 // header file contain all the functions of boost
 //#include <boost/algorithm/string.hpp>
-
+//Congestion- how many nets are within in a region.
+//
 
 int main() {
 	vector<vector<Cell>> PlacementGrid;
+    vector<Cell>Initializer;
     
     ifstream Cellfile("file.txt");
+    ifstream NetFile("netFile.txt");
+
+    string line;
+    while (getline(Cellfile, line))
+    {
+        istringstream iss(line);
+        string CellName;
+        int area;
+        if (!(iss >> CellName>> area)) { break; } // error
+        iss >> CellName >> area;
+        Initializer.push_back(Cell(CellName, area));
+
+        // process pair (a,b)
+    }
 
     /*int start(0), end(0);
     char dl = ' ';
