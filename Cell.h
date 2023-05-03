90cdb608810e0f9d04c5663077284dcd32be151a
Elijah Farrell <84297215+vroelijah@users.noreply.github.com>
2023-05-03 19:09:53 -0400

    Added to main

--- Cell.h
+++ Cell.h
@@ -9,24 +9,36 @@ class Cell {
 	const string name;
 	vector<int> currentPos = {};
 	const int area;
+	int x, y=0;
 
 
 public:
 
-	Cell(string& name, int &area) :name(name), area(area) {}
-	Cell(string& name, int &x, int& y, const int &area) :name(name), currentPos({ x,y }), area(area) {}
+	Cell(string& name, int &area) :name(name), area(area),x(0),y(0) {}
+	Cell(string& name, int &x, int& y, const int &area) :name(name), x(x),y(y), area(area) {}
 
 	//Functions
 	const string getName() {
 		return this->name;
 	}
 
-	vector<int>getPosition() {
-		return this->currentPos;
+	int getX() {
+		return this->x;
+	}
+	int getY() {
+		return this->y;
 	}
 
 	const int getArea() {
 		return this->area;
+	}
+
+	void setX(int &x) {
+		this->x = x;
+	}
+	void setY(int &y) {
+		this->y = y;
+
 	}
 
 	//mutate functions
--- main.cpp
+++ main.cpp
@@ -4,11 +4,24 @@
 #include <string>
 #include <fstream>
 using namespace std;
+// header file contain all the functions of boost
+//#include <boost/algorithm/string.hpp>
 
 
 int main() {
 	vector<vector<Cell>> PlacementGrid;
+    
+    ifstream Cellfile("file.txt");
 
+    /*int start(0), end(0);
+    char dl = ' ';
+
+    int a;
+    string b;
+    while (Cellfile >> a >> b)
+    {
+        
+    }*/
 
 
 
