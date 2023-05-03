93a524bcd9123d5f9948e4d822f1d04c061c1e7f
Elijah Farrell <84297215+vroelijah@users.noreply.github.com>
2023-05-03 15:06:48 -0400

    added changes to nets class

--- Cell.h
+++ Cell.h
@@ -16,6 +16,26 @@ public:
 	Cell(string& name, int &area) :name(name), area(area) {}
 	Cell(string& name, int &x, int& y, const int &area) :name(name), currentPos({ x,y }), area(area) {}
 
+	//Functions
+	const string getName() {
+		return this->name;
+	}
+
+	vector<int>getPosition() {
+		return this->currentPos;
+	}
+
+	const int getArea() {
+		return this->area;
+	}
+
+	//mutate functions
+	void setCurrentPos(int x, int y) {
+		if (currentPos.empty()) {
+			currentPos.push_back(x);
+			currentPos.push_back(y);
+		}
+	}
 
 
 
--- Nets.h
+++ Nets.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <string>
+#include <vector>
+#include "Cell.h"
+using namespace std;
+
+
+class Net {
+
+	 Cell startingCell;
+	 vector<Cell>neighborCells;
+
+public:
+
+	Net( Cell &start,vector<Cell>neighbors) :startingCell(start), neighborCells(neighbors) {}
+
+	const string getNetStartCellName() {
+		return this->startingCell.getName();
+	}
+
+};
